f46a7de5f16ed468a911bdce70a4e04fd8ec157b
TaiTau <manbatai2000@gmail.com>
2022-01-01 08:52:55 +0700

    demo

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Tesseract
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btChange = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.ptb1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ListLineImage = new System.Windows.Forms.ListBox();
+            this.ListImagePass = new System.Windows.Forms.ListBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btDel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn thư mục chứa các ảnh 1 dòng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(468, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Danh sách ảnh đã sửa";
+            this.label2.Enter += new System.EventHandler(this.label2_Enter);
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(330, 58);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(54, 23);
+            this.btOpen.TabIndex = 2;
+            this.btOpen.Text = "Mở";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btChange
+            // 
+            this.btChange.Location = new System.Drawing.Point(390, 297);
+            this.btChange.Name = "btChange";
+            this.btChange.Size = new System.Drawing.Size(75, 23);
+            this.btChange.TabIndex = 3;
+            this.btChange.Text = "<<";
+            this.btChange.UseVisualStyleBackColor = true;
+            this.btChange.Click += new System.EventHandler(this.btChange_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(858, 85);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(87, 31);
+            this.btExit.TabIndex = 4;
+            this.btExit.Text = "Thoát";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // ptb1
+            // 
+            this.ptb1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("ptb1.ErrorImage")));
+            this.ptb1.Location = new System.Drawing.Point(30, 564);
+            this.ptb1.Name = "ptb1";
+            this.ptb1.Size = new System.Drawing.Size(795, 56);
+            this.ptb1.TabIndex = 7;
+            this.ptb1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 546);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ảnh 1 dòng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 649);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(30, 673);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(795, 26);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ListLineImage
+            // 
+            this.ListLineImage.FormattingEnabled = true;
+            this.ListLineImage.Location = new System.Drawing.Point(30, 85);
+            this.ListLineImage.Name = "ListLineImage";
+            this.ListLineImage.Size = new System.Drawing.Size(354, 446);
+            this.ListLineImage.TabIndex = 11;
+            this.ListLineImage.Click += new System.EventHandler(this.listBox1_Click);
+            this.ListLineImage.SelectedIndexChanged += new System.EventHandler(this.ListLineImage_SelectedIndexChanged);
+            // 
+            // ListImagePass
+            // 
+            this.ListImagePass.FormattingEnabled = true;
+            this.ListImagePass.Location = new System.Drawing.Point(471, 85);
+            this.ListImagePass.Name = "ListImagePass";
+            this.ListImagePass.Size = new System.Drawing.Size(354, 446);
+            this.ListImagePass.TabIndex = 12;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btDel
+            // 
+            this.btDel.Location = new System.Drawing.Point(858, 129);
+            this.btDel.Name = "btDel";
+            this.btDel.Size = new System.Drawing.Size(87, 33);
+            this.btDel.TabIndex = 14;
+            this.btDel.Text = "Xóa";
+            this.btDel.UseVisualStyleBackColor = true;
+            this.btDel.Click += new System.EventHandler(this.btDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 736);
+            this.Controls.Add(this.btDel);
+            this.Controls.Add(this.ListImagePass);
+            this.Controls.Add(this.ListLineImage);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ptb1);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btChange);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chỉnh sửa kết quả tesseract";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btChange;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.PictureBox ptb1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox ListLineImage;
+        private System.Windows.Forms.ListBox ListImagePass;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btDel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Security.Policy;
+using System.Collections;
+using System.Linq;
+
+namespace Tesseract
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        public string result1;
+        public string[] arr2 = new string[100];
+        public int j=0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        public String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+
+            
+
+            ///dưới này là code của tài
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath, "*.jpg", SearchOption.AllDirectories);
+                    foreach (string fileName in files)
+                    {
+                        ListLineImage.Items.Add(fileName);
+                    }
+                    
+   
+                }
+               
+            }
+  
+            string filePath = @"D:/Tesseract/data/segment2/image_check.txt";
+
+            string[] lines;
+            string str;
+
+            if (System.IO.File.Exists(filePath))
+            {
+                lines = System.IO.File.ReadAllLines(filePath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    
+                    ListLineImage.Items.Remove(lines[i]);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("File does not exist");
+            }
+
+            
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            string path2 = @"D:/Tesseract/data/segment2/image_check.txt";
+            while (arr2[0] != "")
+            {
+                using (StreamWriter sw = new StreamWriter(path2, true))
+                {
+                    sw.WriteLine(arr2[j - 1].ToString());
+                }
+                arr2[j - 1] = "";
+                j = j - 1;
+            }
+            j = j + 1;
+            this.Close();
+        }
+
+        public void listBox1_Click(object sender, EventArgs e)
+        {         
+            //result1 = Path.GetFileNameWithoutExtension(ListLineImage.SelectedItem.ToString());// bỏ đuôi
+            if (ListLineImage.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("chưa chọn item");
+            }
+            else
+            {           
+                result1 = Path.GetFileNameWithoutExtension(ListLineImage.SelectedItem.ToString());// bỏ đuôi
+                string path = ListLineImage.SelectedItem.ToString();  
+
+                Image img = Image.FromFile(path);
+                ptb1.Image = img;          
+
+                //////xuất file txt
+                string filePath = @"D:/Tesseract/data/segment2/" + result1 + ".txt";
+                string str;
+
+                if (System.IO.File.Exists(filePath))
+                {
+                    str = System.IO.File.ReadAllText(filePath);
+                    textBox1.Text = str;
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                    textBox1.Focus();
+                }
+                else
+                {
+                   MessageBox.Show("File does not exist");
+                }
+                
+
+            }
+            
+             
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+ 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListLineImage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btDel_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                textBox1.Focus();
+                if (ListLineImage.SelectedItems.Count == 0)
+                {
+                    MessageBox.Show("chưa chọn item");
+                }
+                else
+                {
+                    string result1 = Path.GetFileNameWithoutExtension(ListLineImage.SelectedItem.ToString());// bỏ đuôi
+                    string path1 = ListLineImage.SelectedItem.ToString();
+                    string path2 = @"D:/Tesseract/data/segment2/image_check.txt";
+                    string fileLPath = @"D:/Tesseract/data/segment2/" + result1 + ".txt";
+                    string str;
+                    str = textBox1.Text;
+                    System.IO.File.WriteAllText(fileLPath, str);
+
+
+                    if (ListLineImage.SelectedItems.Count != 0)
+                    {  
+                        arr2[j] = ListLineImage.SelectedItem.ToString();
+                        ListLineImage.Items.Remove(ListLineImage.SelectedItems[0]);
+                        ListImagePass.Items.Add(path1);
+                        j = j + 1;
+                    }
+
+                    ListLineImage.SelectedIndex = 0;
+                    Image img2 = Image.FromFile(ListLineImage.SelectedItem.ToString());
+                    ptb1.Image = img2;
+
+                    //////xuất file txt
+
+                    string str2;
+                    string result2 = Path.GetFileNameWithoutExtension(ListLineImage.SelectedItem.ToString());// bỏ đuôi
+                    string filePath2 = @"D:/Tesseract/data/segment2/" + result2 + ".txt";
+                    if (System.IO.File.Exists(filePath2))
+                    {
+                        str2 = System.IO.File.ReadAllText(filePath2);
+                        textBox1.Text = str2;
+                        textBox1.SelectionStart = textBox1.Text.Length;
+                        textBox1.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("File does not exist");
+                    }
+
+
+                }
+
+            }
+        }
+
+        private void btChange_Click(object sender, EventArgs e)
+        {
+            
+
+            if (ListImagePass.SelectedItem == null)
+            {
+                MessageBox.Show("Chưa chọn Item");
+            }
+            else
+            {
+                ListLineImage.Items.Add(ListImagePass.SelectedItem);
+                ListImagePass.Items.Remove(ListImagePass.SelectedItem);
+            }
+
+        }
+    }
+}
